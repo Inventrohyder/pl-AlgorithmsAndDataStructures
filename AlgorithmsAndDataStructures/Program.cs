8d278ed5650bf5f80d98c1a0c606f333a131703a
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-07-13 21:35:28 +0300

    feat: implement a postfix calculator using LIFO

--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -1,48 +1,65 @@
-﻿using NodeChains;
+﻿using Stack.List;
 using System;
 
 namespace Calculator
 {
-    class Program
+class Program
     {
-        static void Main(string[] args)
-        {
-            //*.....*......*
-            // |  3  | null *
-            // *.....*......*
-            Node first = new Node { Value = 3 };
+       // calc.exec 5 6 7 * + 1 -
+       static void Main(string[] args)
+       {
+           // The stack of integers not yet operated on
+           Stack<int> values = new Stack<int>();
 
-            // *.....*......*    *.....*......*
-            // |  3  | null *    |  5  | null *
-            // *.....*......*    *.....*......*
-            Node middle = new Node { Value = 5 };
+           String[] arguments = getArguments();
 
-            // *.....*......*    *.....*......*
-            // |  3  |  *...*...>|  5  | null *
-            // *.....*......*    *.....*......*
-            first.Next = middle;
+           foreach (string token in arguments)
+           {
+               // if the value is an integer...
+               int value;
+               if (int.TryParse(token, out value))
+               {
+                   // ... push it to the stack
+                   values.Push(value);
+               }
+               else
+               {
+                   // otherwise evaluate the expression...
+                   int rhs = values.Pop();
+                   int lhs = values.Pop();
 
-            // *.....*......*    *.....*......*    *.....*......*
-            // |  3  |  *...*...>|  5  | null *    |  7  | null *
-            // *.....*......*    *.....*......*    *.....*......*
-            Node last = new Node { Value = 7 };
+                   // ... and push the result back to the stack
+                   switch (token)
+                   {
+                       case "+":
+                           values.Push(lhs + rhs);
+                           break;
+                       case "-":
+                           values.Push(lhs - rhs);
+                           break;
+                       case "*":
+                           values.Push(lhs * rhs);
+                           break;
+                       case "/":
+                           values.Push(lhs / rhs);
+                           break;
+                       case "%":
+                           values.Push(lhs % rhs);
+                           break;
+                       default:
+                           throw new ArgumentException(string.Format("Unexpected token {0}", token));
+                   }
+               }
+           }
 
-            // *.....*......*    *.....*......*    *.....*......*
-            // |  3  |  *...*...>|  5  |  *...*...>|  7  | null *
-            // *.....*......*    *.....*......*    *.....*......*
-            middle.Next = last;
+           // the last item on the stack is the result
+           Console.WriteLine(values.Pop());
+       }
 
-            // iterate over each node and print the value
-            PrintList(first);
-        }
-
-        private static void PrintList(Node node)
-        {
-            while (node != null)
-            {
-                Console.WriteLine(node.Value);
-                node = node.Next;
-            }
-        }
+       private static string[] getArguments()
+       {
+           String operation = Console.ReadLine();
+           return operation.Split();
+       }
     }
 }
