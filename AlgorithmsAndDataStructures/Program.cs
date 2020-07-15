b5d38088bd905d5191a59ad4175d9397284f447d
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-07-15 20:43:04 +0300

    🛠fix: rearrange code to individual projects

--- AlgorithmsAndDataStructures/Program.cs
+++ AlgorithmsAndDataStructures/Program.cs
@@ -1,65 +1,45 @@
-﻿using Stack.List;
-using System;
+﻿using System;
+using NodeChains;
 
-namespace Calculator
-{
 class Program
+{
+    static void Main(string[] args)
     {
-       // calc.exec 5 6 7 * + 1 -
-       static void Main(string[] args)
-       {
-           // The stack of integers not yet operated on
-           Stack<int> values = new Stack<int>();
+        //*.....*......*
+        // |  3  | null *
+        // *.....*......*
+        Node first = new Node { Value = 3 };
 
-           String[] arguments = getArguments();
+        // *.....*......*    *.....*......*
+        // |  3  | null *    |  5  | null *
+        // *.....*......*    *.....*......*
+        Node middle = new Node { Value = 5 };
 
-           foreach (string token in arguments)
-           {
-               // if the value is an integer...
-               int value;
-               if (int.TryParse(token, out value))
-               {
-                   // ... push it to the stack
-                   values.Push(value);
-               }
-               else
-               {
-                   // otherwise evaluate the expression...
-                   int rhs = values.Pop();
-                   int lhs = values.Pop();
+        // *.....*......*    *.....*......*
+        // |  3  |  *...*...>|  5  | null *
+        // *.....*......*    *.....*......*
+        first.Next = middle;
 
-                   // ... and push the result back to the stack
-                   switch (token)
-                   {
-                       case "+":
-                           values.Push(lhs + rhs);
-                           break;
-                       case "-":
-                           values.Push(lhs - rhs);
-                           break;
-                       case "*":
-                           values.Push(lhs * rhs);
-                           break;
-                       case "/":
-                           values.Push(lhs / rhs);
-                           break;
-                       case "%":
-                           values.Push(lhs % rhs);
-                           break;
-                       default:
-                           throw new ArgumentException(string.Format("Unexpected token {0}", token));
-                   }
-               }
-           }
+        // *.....*......*    *.....*......*    *.....*......*
+        // |  3  |  *...*...>|  5  | null *    |  7  | null *
+        // *.....*......*    *.....*......*    *.....*......*
+        Node last = new Node { Value = 7 };
 
-           // the last item on the stack is the result
-           Console.WriteLine(values.Pop());
-       }
+        // *.....*......*    *.....*......*    *.....*......*
+        // |  3  |  *...*...>|  5  |  *...*...>|  7  | null *
+        // *.....*......*    *.....*......*    *.....*......*
+        middle.Next = last;
 
-       private static string[] getArguments()
-       {
-           String operation = Console.ReadLine();
-           return operation.Split();
-       }
+        // iterate over each node and print the value
+        PrintList(first);
+    }
+
+    private static void PrintList(Node node)
+    {
+        while (node != null)
+        {
+            Console.WriteLine(node.Value);
+            node = node.Next;
+        }
     }
 }
---
+++ Stack/Program.cs
@@ -0,0 +1,66 @@
+﻿using Stack.List;
+using System;
+
+namespace Calculator
+{
+class Program
+    {
+       // calc.exec 5 6 7 * + 1 -
+       static void Main(string[] args)
+       {
+           // The stack of integers not yet operated on
+           Stack<int> values = new Stack<int>();
+
+           String[] arguments = getArguments();
+
+           foreach (string token in arguments)
+           {
+               // if the value is an integer...
+               int value;
+               if (int.TryParse(token, out value))
+               {
+                   // ... push it to the stack
+                   values.Push(value);
+               }
+               else
+               {
+                   // otherwise evaluate the expression...
+                   int rhs = values.Pop();
+                   int lhs = values.Pop();
+
+                   // ... and push the result back to the stack
+                   switch (token)
+                   {
+                       case "+":
+                           values.Push(lhs + rhs);
+                           break;
+                       case "-":
+                           values.Push(lhs - rhs);
+                           break;
+                       case "*":
+                           values.Push(lhs * rhs);
+                           break;
+                       case "/":
+                           values.Push(lhs / rhs);
+                           break;
+                       case "%":
+                           values.Push(lhs % rhs);
+                           break;
+                       default:
+                           throw new ArgumentException(string.Format("Unexpected token {0}", token));
+                   }
+               }
+           }
+
+            // the last item on the stack is the result
+            Console.WriteLine(string.Format("The answer is: {0}", values.Pop()));
+       }
+
+       private static string[] getArguments()
+       {
+            Console.Write("Enter the postfix operation to carry out: ");
+           String operation = Console.ReadLine();
+           return operation.Split();
+       }
+    }
+}
