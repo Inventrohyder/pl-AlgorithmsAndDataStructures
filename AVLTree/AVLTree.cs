f8188e80fe6fdb092aa0fa39cdfb4538a9e8f845
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-08-15 05:13:55 +0300

    ✨ feat: add base classes for AVL tree implementation

---
+++ AVLTree/AVLTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AVLTree
+{
+    public class AVLTree<T> : IEnumerable<T>
+        where T : IComparable
+    {
+        public AVLTreeNode<T> Head { get; }
+
+        public void Add(T value)
+        {
+
+        }
+        public bool Remove(T value)
+        {
+
+        }
+
+        public bool Contains(T value)
+        {
+
+        }
+        public void Clear()
+        {
+
+        }
+        public int Count { get; }
+
+        public void PreOrderTraversal(Action<T> action)
+        {
+
+        }
+        public void PostOrderTraversal(Action<T> action)
+        {
+
+        }
+        public void InOrderTraversal(Action<T> action)
+        {
+
+        }
+
+        public IEnumerator<T> InOrderTraversal()
+        {
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AVLTree/AVLTreeNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AVLTree
+{
+    public class AVLTreeNode<TNode> : IComparable<TNode>
+        where TNode : IComparable
+    {
+        public AVLTreeNode(TNode value, AVLTreeNode<TNode> parent, AVLTree<TNode> tree)
+        {
+        }
+
+        public AVLTreeNode<TNode> Left { get; private set; }
+        public AVLTreeNode<TNode> Right { get; private set; }
+        public AVLTreeNode<TNode> Parent { get; private set; }
+        public TNode value { get; private set; }
+
+        public int CompareTo([AllowNull] TNode other)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Balancing Methods
+        internal void Balance() { }
+        private void LeftRotation() { }
+        private void RightRotation() { }
+        private void LeftRightRotation() { }
+        private void RightLeftRotation() { }
+
+        // Support properties and methods
+        private int MaxChildHeight(AVLTreeNode<TNode> node) { }
+        private int LeftHeight { get; }
+        private int RightHeight { get; }
+        private TreeState State { get; }
+        private int BalanceFactor { get; }
+    }
+}
