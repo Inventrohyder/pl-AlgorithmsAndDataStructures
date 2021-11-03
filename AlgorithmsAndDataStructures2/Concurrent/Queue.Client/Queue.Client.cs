17f6f56209f8aff5797ed4b4cad2c4b2a5467133
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2021-11-03 02:46:00 +0100

    ✨ feat: concurrency

---
+++ AlgorithmsAndDataStructures2/Concurrent/Queue.Client/Queue.Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Queue.Common;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Queue.Client
+{
+    public class Client
+    {
+        public static int Main()
+        {
+            List<IQueue<Job>> queues = new List<IQueue<Job>>()
+            {
+                new Queue.Locking.PriorityQueue<Job>(),
+                new Queue.ReadWriteLock.PriorityQueue<Job>(),
+            };
+
+            foreach (IQueue<Job> queue in queues)
+            {
+                Console.WriteLine("STARTING {0}.{1}", queue.GetType().Namespace, queue.GetType().Name);
+
+                JobProcessor processor = new JobProcessor(queue);
+                processor.Load(500000);
+                
+                Stopwatch processTimer = Stopwatch.StartNew();
+
+                // start the concurrent job processing
+                processor.ProcessInParallel(4);
+
+                while(processor.Remaining > 0)
+                {
+                    Console.WriteLine($"{processor.Remaining} : {processor.Completed}");
+                    Thread.Sleep(1000);
+                }
+
+                processor.Wait();
+
+                processTimer.Stop();
+
+                if (processor.Exception != null)
+                {
+                    Console.WriteLine("ERROR {0}", processor.Exception.Message);
+                }
+                else
+                {
+                    Console.WriteLine("SUCCESS Process: {0}ms", processTimer.ElapsedMilliseconds);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures2/Concurrent/Queue.Common/Job.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Queue.Common
+{
+    public class Job : IComparable<Job>
+    {
+        // indicates the number of times Process has been called
+        int performed = 0;
+
+        readonly int Priority;
+
+        public Job(int priority)
+        {
+            Priority = priority;
+        }
+
+        public int CompareTo(Job other)
+        {
+            return Priority.CompareTo(other?.Priority);
+        }
+
+        public void Process()
+        {
+            // If Process is called more than once then throw an exception
+            if (Interlocked.Increment(ref performed) > 1)
+            {
+                throw new InvalidOperationException("Action performed multiple times");
+            }
+        }
+    }
+}
